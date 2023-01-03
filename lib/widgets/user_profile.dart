a3e1d0a7608b529f6c99fdb8fb21d8f2f699deb5
ihorid <dovbenkoin@gmail.com>
2023-01-03 14:43:36 +0200

    chenge SizedBox

--- lib/widgets/user_profile.dart
+++ lib/widgets/user_profile.dart
@@ -14,8 +14,8 @@ class _ExempleState extends State<Exemple> {
   Widget build(BuildContext context) {
     return const Center(
       child: SizedBox(
-        width: 100,
-        height: 100,
+        width: 200,
+        height: 200,
         child: RadialPersentWidget(
           percent: 0.72,
           fillColor: Colors.black,
